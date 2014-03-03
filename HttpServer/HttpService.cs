9a699297a6eaf373a0d6490ef52ff45cabbba30d
Donatas <donce.lt@gmail.com>
2014-03-03 10:52:05 +0100

    HttpService

---
+++ HttpServer/HttpService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpServer
+{
+    class HttpService
+    {
+        private TcpClient client;
+
+        public HttpService(TcpClient client)
+        {
+            this.client = client;
+        }
+
+        public void Process()
+        {
+            //TODO: process request
+        }
+
+    }
+}

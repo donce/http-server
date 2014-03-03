f3b4ea2ffcbcbced395cca4e1949dc2a887d077d
Donatas <donce.lt@gmail.com>
2014-03-03 11:15:43 +0100

    Concurrency.

--- HttpServer/HttpService.cs
+++ HttpServer/HttpService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
@@ -18,7 +19,40 @@ namespace HttpServer
 
         public void Process()
         {
-            //TODO: process request
+            Console.WriteLine("Client connected");
+            Stream stream = client.GetStream();
+            StreamReader reader = new StreamReader(stream);
+            StreamWriter writer = new StreamWriter(stream);
+
+            string line = reader.ReadLine();
+            //                while (line != null)
+            //                {
+            Console.WriteLine(line);
+            //                    line = reader.ReadLine();
+            //                }
+
+            string filename = GetFile(line);
+
+            string content = "You've requested " + filename;
+            writer.WriteLine("HTTP/1.0 200 OK");
+            writer.WriteLine("");
+            writer.WriteLine(content);
+
+            writer.Flush();
+
+            client.Close();//TODO: in finally
+        }
+
+        public string GetFile(string getRequest)
+        {
+            string[] requestWords = getRequest.Split(' ');
+            if (!requestWords[0].Equals("GET"))
+            {
+                throw (new ArgumentException("Only GET requests are supported"));
+            }
+            string pathToFile = requestWords[1];
+            return pathToFile;
+            //            return "index.html";
         }
 
     }
--- HttpServer/Server.cs
+++ HttpServer/Server.cs
@@ -18,41 +18,10 @@ namespace HttpServer
             while (true)
             {
                 TcpClient client = listener.AcceptTcpClient();
-                Console.WriteLine("Client connected");
-                Stream stream = client.GetStream();
-                StreamReader reader = new StreamReader(stream);
-                StreamWriter writer = new StreamWriter(stream);
-
-                string line = reader.ReadLine();
-//                while (line != null)
-//                {
-                    Console.WriteLine(line);
-//                    line = reader.ReadLine();
-//                }
-
-                string filename = GetFile(line);
-
-                string content = "You've requested " + filename;
-                writer.WriteLine("HTTP/1.0 200 OK");
-                writer.WriteLine("");
-                writer.WriteLine(content);
-
-                writer.Flush();
-
-                client.Close();//TODO: in finally
+                HttpService service = new HttpService(client);
+                Task.Factory.StartNew(service.Process);
             }
         }
 
-        public static string GetFile(string getRequest)
-        {
-            string[] requestWords = getRequest.Split(' ');
-            if (!requestWords[0].Equals("GET"))
-            {
-                throw (new ArgumentException("Only GET requests are supported"));
-            }
-            string pathToFile = requestWords[1];
-            return pathToFile;
-//            return "index.html";
-        }
     }
 }

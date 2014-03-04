832c48ae751707b3384590f9bab9bb461bfc150e
Erik <erik.kisel@gmail.com>
2014-03-04 12:01:28 +0100

    Separate server class

--- HttpServer/Server.cs
+++ HttpServer/Server.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Net.Sockets;
-using System.Reflection;
-using System.Threading.Tasks;
-using log4net;
-
+﻿
 namespace HttpServer
 {
     
@@ -14,31 +9,8 @@ namespace HttpServer
 
         static void Main(string[] args)
         {
-            Console.CancelKeyPress += delegate(object sender, ConsoleCancelEventArgs e)
-            {
-                e.Cancel = true;
-                Console.WriteLine("CLOSING");//TODO: log
-                accepting = false;
-            };
-
-            log4net.Config.XmlConfigurator.Configure();
-            ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
-            TcpListener listener = new TcpListener(DefaultPort);
-            listener.Start();
-            log.Info("Server has started");
-
-
-            //TODO: graceful
-            while (accepting)
-            {
-                TcpClient client = listener.AcceptTcpClient();
-                log.Info("New client accepted");
-                HttpService service = new HttpService(client);
-                Action action = new Action(service.Process);
-                log.Info("Sending reponse to client");
-                Task.Run(action);
-                log.Info("Response sent");
-            }
+            ServerClass server = new ServerClass();
+            server.Start(DefaultPort);//TODO:port in config file
         }
     }
 }
---
+++ HttpServer/ServerClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Threading.Tasks;
+using log4net;
+
+namespace HttpServer
+{
+    class ServerClass
+    {
+        private int _port;
+        private static bool _accepting = true;
+
+        public void Start(int port)
+        {
+            log4net.Config.XmlConfigurator.Configure();
+            ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+            Console.CancelKeyPress += delegate(object sender, ConsoleCancelEventArgs e)
+            {
+                e.Cancel = true;
+                Console.WriteLine("CLOSING");
+                log.Warn("Server closing");
+                _accepting = false;
+            };
+
+            TcpListener listener = new TcpListener(port);
+            listener.Start();
+            log.Info("Server has started");
+
+            while (_accepting)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                log.Info("New client accepted");
+                HttpService service = new HttpService(client);
+                Action action = new Action(service.Process);
+                log.Info("Sending reponse to client");
+                Task.Run(action);
+                log.Info("Response sent");
+            }
+        }
+        //TODO:Stop method
+    }
+}
